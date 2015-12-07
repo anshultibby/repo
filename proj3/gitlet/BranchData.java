c8e5c24fd14ac1aff746746a9aa7d66f5b1d7d71
Anshul Tibrewal <Anshul22@berkeley.edu>
2015-12-06 20:11:03 -0800

    done finished fini wooohoooo

--- proj3/gitlet/BranchData.java
+++ proj3/gitlet/BranchData.java
@@ -7,37 +7,54 @@ import java.util.HashMap;
 
 public class BranchData implements Serializable{
     
-	public BranchData(){ 
+    public BranchData(){ 
         _branches = new HashMap<String, String>();
         _untracked = new ArrayList<String>();
-    }
-	
-    public BranchData(String Branch, String Commit) {
-    	_branches = new HashMap<String, String>();
-    	_untracked = new ArrayList<String>();
-    	_branches.put(Branch, Commit);
-    	_current = Branch;
+        _remotes = new HashMap<String, String>();
+        _remotes.put("local", ".");
     }
     
+    public BranchData(String Branch, String Commit) {
+        _branches = new HashMap<String, String>();
+        _remotes = new HashMap<String, String>();
+        _untracked = new ArrayList<String>();
+        _branches.put(Branch, Commit);
+        _current = Branch;
+        _remotes.put("local", ".");
+    }
+    public boolean hasremote(String remotename) {
+        return _remotes.containsKey(remotename);
+    }
+    public void addremote(String remotename, String remotepath) {
+        remotepath.replaceAll("\\/" ,  java.io.File.separator);
+        _remotes.put(remotename, remotepath);
+        
+    }
+    public void rmremote(String name) {
+        _remotes.remove(name);
+    }
+    public String getremotepath(String name) {
+        return _remotes.get(name);
+    }
     public void setcurrent(String Branch) {
-    	_current = Branch;
+        _current = Branch;
     }
     
     public void setcurrhead(String commit) {
-    	_branches.put(_current, commit);
+        _branches.put(_current, commit);
     }
     
     public void addbranch(String Branch, String Commit) {
-    	_branches.put(Branch, Commit);
+        _branches.put(Branch, Commit);
     }
     
     public void addcurrbranch(String Branch, String Commit) {
-    	_branches.put(Branch, Commit);
-    	_current = Branch;
+        _branches.put(Branch, Commit);
+        _current = Branch;
     }
     
     public void addcommit(String Commit) {
-    	_branches.put(_current, Commit);
+        _branches.put(_current, Commit);
     }
     
     public String getcommitname(String branch) {
@@ -50,77 +67,78 @@ public class BranchData implements Serializable{
     
     /** Returns the SHA name of the head commit of the current branch. */
     public String getcurrhead() {
-    	return _branches.get(_current);
+        return _branches.get(_current);
     }
     
     /** Returns true if the entered string is the current branch that we are at. */
     public boolean iscurrent(String branch) {
-    	return branch.equals(_current);
+        return branch.equals(_current);
     }
     
     /** Removes the given branch returning true if it succeeds and false if the branch doesn't exist. */
     public boolean removebranch(String branch) {
-    	if (_branches.containsKey(branch)) {
-    		_branches.remove(branch);
-    		return true;
-    	}
-    	return false;
+        if (_branches.containsKey(branch)) {
+            _branches.remove(branch);
+            return true;
+        }
+        return false;
     }
     
     /** Boolean returns true if the branch data contains the given branch. */
     public boolean containsbranch(String branch) {
-    	return _branches.containsKey(branch);
+        return _branches.containsKey(branch);
     }
     
     /** Returns the commit object which the current head pointer is at. */
     public Commit getcurrobj() {
-    	String commit = _branches.get(_current);
-    	File commitf = new File(".gitlet/.commits", commit);
-    	Commit commitobj = Main.getcommitobject(commitf);
-    	return commitobj;
+        String commit = _branches.get(_current);
+        File commitf = new File(".gitlet/.commits", commit);
+        Commit commitobj = Main.getcommitobject(commitf);
+        return commitobj;
     }
     
     /** Returns the commit object which the particular branche's head pointer is at. */
     public Commit getcommitobj(String branch) {
-    	String commit = _branches.get(branch);
-    	File commitf = new File(".gitlet/.commits", commit);
-    	Commit commitobj = Main.getcommitobject(commitf);
-    	return commitobj;
+        String commit = _branches.get(branch);
+        File commitf = new File(".gitlet/.commits", commit);
+        Commit commitobj = Main.getcommitobject(commitf);
+        return commitobj;
     }
     
     /** Returns the number of branches that exist in this folder. */
     public int size() {
-    	return _branches.size();
+        return _branches.size();
     }
     
     /** Returns the name of the current branch that we are at. */
     public String getcurrent() {
-    	return _current;
+        return _current;
     }
     /** Adds a file to be untracked by the next commit because of remove. */
     public void untrack(String filename) {
-   	 _untracked.add(filename);
+     _untracked.add(filename);
     }
     /** Boolean for whether the untracked arraylist contain this particular file. */
     public boolean contains(String filename) {
-    	if (_untracked.contains(filename)) {
-    		_untracked.remove(filename);
-    		return true;
-    	}
-		return false;
+        if (_untracked.contains(filename)) {
+            _untracked.remove(filename);
+            return true;
+        }
+        return false;
     }
     /** Boolean for whether any files have been marked for being untracked. */
     public boolean hasuntracked() {
-    	if (_untracked.size() == 0) {
-    		return false;
-    	}
-    	return true;
+        if (_untracked.size() == 0) {
+            return false;
+        }
+        return true;
     }
     
     public HashMap<String, String> getBranches() {
         return _branches;
     }
-    
+    /** A HashMap which keeps track of all remote branches and the local branch. */
+    private HashMap<String, String> _remotes;
     /** A private list which contains the name of all the files which have been untracked. */
     private ArrayList<String> _untracked;
     /** Private variable which stores the name (the key reference for our map) of the current branch. */
@@ -128,4 +146,5 @@ public class BranchData implements Serializable{
     /** Private HashMap where the keys are the names of the branches and references 
      * are names of the commit files at the head pointer of the given branch. */
     private HashMap<String, String> _branches;
+
 }
--- proj3/gitlet/Commit.java
+++ proj3/gitlet/Commit.java
@@ -9,53 +9,58 @@ import java.util.HashMap;
 
 public class Commit implements Serializable {
     
-	Commit(String commitmessage, String previous) {
-		_timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
-		_commitmessage = commitmessage;
-		_previouscommit = previous;
+    Commit(String commitmessage, String previous) {
+        _timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+        _commitmessage = commitmessage;
+        _previouscommit = previous;
         _blobs = new HashMap<String, String>();
-	}
-	public void add(String nameoffile, String hashcode) {
-		_blobs.put(nameoffile, hashcode);
-	}
-	public String timestamp() {
-		return _timestamp;
-	}
-	public String commitmessage() {
-		return _commitmessage;
-	}
-	public String prev() {
-		return _previouscommit;
-	}
-	public boolean hasmessage(String message) {
-		return _commitmessage.equals(message);
-	}
-	public boolean contains(String name) {
-		if (_blobs.containsKey(name)) {
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean haspreviouscommit() {
-	    return _previouscommit != null;
-	}
-	public Commit prevobj() {
-		File prev = new File(".gitlet/.commits", _previouscommit);
-		Commit prevobj = Main.getcommitobject(prev);
-		return prevobj;
-	}
-	
-	public HashMap<String, String> getmap() {
-		return _blobs;
-	}
-	
+    }
+    public void add(String nameoffile, String hashcode) {
+        _blobs.put(nameoffile, hashcode);
+    }
+    public String timestamp() {
+        return _timestamp;
+    }
+    public String commitmessage() {
+        return _commitmessage;
+    }
+    public String prev() {
+        return _previouscommit;
+    }
+    public boolean hasmessage(String message) {
+        return _commitmessage.equals(message);
+    }
+    public boolean contains(String name) {
+        if (_blobs.containsKey(name)) {
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean haspreviouscommit() {
+        return _previouscommit != null;
+    }
+    public Commit prevobj() {
+        File prev = new File(".gitlet/.commits", _previouscommit);
+        Commit prevobj = Main.getcommitobject(prev);
+        return prevobj;
+    }
+    public Commit prevremoteobj(String remote) {
+        File remotef = new File(remote, ".gitlet/.commits");
+        File prev = new File(remotef, _previouscommit);
+        Commit prevobj = Main.getcommitobject(prev);
+        return prevobj;
+    }
+    public HashMap<String, String> getmap() {
+        return _blobs;
+    }
+    
     /** Private date variable which stores the timestamp. */
-	private String _timestamp;
-	/** Private string variable which contains the commit message. */
-	private String _commitmessage;
-	/** Data structure that keeps track of file names and their SHA1 hashcodes. */
-	private HashMap<String, String> _blobs;
-	/** A String which stores a reference to the previous commit. */
-	private String _previouscommit;
+    private String _timestamp;
+    /** Private string variable which contains the commit message. */
+    private String _commitmessage;
+    /** Data structure that keeps track of file names and their SHA1 hashcodes. */
+    private HashMap<String, String> _blobs;
+    /** A String which stores a reference to the previous commit. */
+    private String _previouscommit;
 }
